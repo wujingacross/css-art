bfad1d90573d7807c25318efcdebd09657247bc4
wujing <wujing@1d1d100.com>
2020-07-12 23:39:30 +0800

    feat: add unit9

--- src/pages/unit9.js
+++ src/pages/unit9.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
-import styles from "./unit8.module.css"
+import styles from "./unit9.css"
 
 export default function Transition() {
   return (
@@ -10,7 +10,11 @@ export default function Transition() {
         缓动transition用于在元素从一个状态转换到另一个状态时，增加中间的过渡效果，使视觉效果平滑改变，并起到吸引用户注意力的作用。
         另外，还可以利用缓动令元素从一个图案动态变幻为另一个截然不同的图案
       </p>
-      <section className={styles.container}></section>
+      <section className={styles.container}>
+        <figure>
+          <div className="hov" />
+        </figure>
+      </section>
     </Layout>
   )
 }
